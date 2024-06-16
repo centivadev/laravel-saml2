0bede477a313770499817e6a09cb84805618a612
David Voinson <david@centiva.ca>
2024-06-16 12:45:33 -0400

    chore: Update SAML2 routes to use Slides\Saml2\Overrides\OneLoginAuth

--- src/Auth.php
+++ src/Auth.php
@@ -2,7 +2,7 @@
 
 namespace Slides\Saml2;
 
-use Slides\Saml2\Overrides\Auth as OneLoginAuth;
+use Slides\Saml2\Overrides\OneLoginAuth;
 use OneLogin\Saml2\Error as OneLoginError;
 use Slides\Saml2\Events\SignedOut;
 use Slides\Saml2\Models\Tenant;
